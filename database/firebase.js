50225338fecd184689f2c762a84df26cc94ce0a8
Alexis J Sanchez <kiritox123@gmail.com>
2021-09-13 20:19:44 -0700

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -4,13 +4,13 @@ import "firebase/firebase-firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBZRs96mbfzSOIewWrdSET8w-b47C4HEHc",
-    authDomain: "gig-wallet.firebaseapp.com",
-    databaseURL: "https://gig-wallet-default-rtdb.firebaseio.com",
-    projectId: "gig-wallet",
-    storageBucket: "gig-wallet.appspot.com",
-    messagingSenderId: "782230832096",
-    appId: "1:782230832096:web:0e2e71e382e36473dea185",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1:",
 };
 
 // Initialize Firebase
